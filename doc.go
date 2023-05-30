ff9b64d165b6d6d8a670cc15fe094a2469f2cbd8
Simon Woidig <simon.woidig@actis.cz>
2023-05-30 13:34:10 +0200

    add module docs

---
+++ doc.go
@@ -0,0 +1,13 @@
+/*
+Golang-training is a simple application created as a training exercise.
+
+Usage:
+
+	golang-training hello
+
+For more information see:
+
+	golang-training help
+*/
+
+package main
